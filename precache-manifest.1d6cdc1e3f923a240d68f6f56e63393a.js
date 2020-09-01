406dec1827c74ea1e3b147b474d45258ed516630
porushyadav <37874551+porushyadav@users.noreply.github.com>
2020-09-01 21:04:22 +0530

    Updates

---
+++ precache-manifest.1d6cdc1e3f923a240d68f6f56e63393a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d7f428fb9d41ad5b909",
+    "url": "/Ecommerce-clone-React/static/css/main.d9da3f75.chunk.css"
+  },
+  {
+    "revision": "4d7f428fb9d41ad5b909",
+    "url": "/Ecommerce-clone-React/static/js/main.cb84294e.chunk.js"
+  },
+  {
+    "revision": "c6b62c63fa44b9e8978b",
+    "url": "/Ecommerce-clone-React/static/js/runtime~main.8deddea4.js"
+  },
+  {
+    "revision": "8dcbb43126ba7ff2fd9f",
+    "url": "/Ecommerce-clone-React/static/css/2.4bebb8b3.chunk.css"
+  },
+  {
+    "revision": "8dcbb43126ba7ff2fd9f",
+    "url": "/Ecommerce-clone-React/static/js/2.56552274.chunk.js"
+  },
+  {
+    "revision": "efdc6bdafbae010a60db47a942877fe4",
+    "url": "/Ecommerce-clone-React/index.html"
+  }
+];
